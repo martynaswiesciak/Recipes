21391d45c628007deec7dd547c29786306b9f489
Martyna <baaalbinka@icloud.com>
2019-12-17 22:01:19 +0100

    Aktualizacja bazy danych

--- src/models/ingredient.js
+++ src/models/ingredient.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 // połączenie z bazą danych
-mongoose.connect('mongodb://localhost/recipes')  
+mongoose.connect('mongodb://pbocian:xxx12345@recipes-sandbox-shard-00-01-rn7eo.mongodb.net:27017,recipes-sandbox-shard-00-00-rn7eo.mongodb.net:27017,recipes-sandbox-shard-00-02-rn7eo.mongodb.net:27017/test?authSource=admin&replicaSet=Recipes-sandbox-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true')  
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err))
 
