d533d471ad8aa81f69571a151678ea89395a1258
Sabina <sabina.topor20@gmail.com>
2019-12-20 21:03:19 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/js/signin.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const User = require('user');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// router.post('/signup',(req,res,next) =>...)
+
+router.post('/login',(req, res, next) => {
+    User.find({ email: req.body.email})
+        .exec()
+        .then(user =>{
+            if(user.length < 1) {
+                return res.status(401).json({
+                    message: "auth failed"
+                })
+            }
+            bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
+                if(err){
+                    return res.status(401).json({
+                        message: "auth failed"
+                    });
+                }
+                if(result){
+                    const token = jwt.sign({_id: user._id }, config.get('jwtPrivateKey'));
+                    res.send(token);
+                    return res.status(200).json({
+                        message: "auth successful"
+                    });
+                }
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
+})
---
+++ src/js/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = mongoose.Schema({
+    nick: { 
+        type: String, 
+        required: true,
+        minlength: 3,
+        maxlength: 255,
+        trim: true },
+    name: { 
+        type: String, 
+        required: true,
+        minlength: 3,
+        maxlength: 255,
+        trim: true },
+    surname: { 
+        type: String, 
+        required: true,
+        minlength: 3,
+        maxlength: 255,
+        trim: true },
+    password: { 
+        type: String, 
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        trim: true }
+});
+module.exports = mongoose.model('User',userSchema)
