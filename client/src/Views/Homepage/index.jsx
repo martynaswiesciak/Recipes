5026874a4c66b4dc19aeb1853aea8375a9e786b8
Sabina <sabina.topor20@gmail.com>
2020-01-06 15:58:15 +0100

    widok logowania

--- client/src/Views/Homepage/index.jsx
+++ client/src/Views/Homepage/index.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Form, Button } from 'semantic-ui-react';
 import { TopMenu } from '../../components/Homepage';
 
 const Home = () => {
   return (
-    <Segment>
-      <TopMenu></TopMenu>
-      <main>
-        <p>
-          
-       </p>
-      </main>
-    </Segment>
+    <Segment.Group>
+      <Segment>
+        <TopMenu></TopMenu>
+      </Segment>
+
+      <Segment>
+        <main>
+          <div>
+            <Form size='large'>
+              <Form.Field label='Username' control='input' placeholder='Username' />
+            </Form>
+            <Form size='large'>
+              <Form.Field type='password' label='Password' control='input' placeholder='Password' />
+            </Form>
+            <Button type='submit'>Submit</Button>
+        </div>
+        </main>
+      </Segment>
+    </Segment.Group>
+    
   );
 };
 
