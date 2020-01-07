7867f81c06cc1f518c4f68ef409151b775784aa2
Sabina <sabina.topor20@gmail.com>
2020-01-07 20:52:29 +0100

    logowanie obiektowo

--- client/src/Views/Homepage/index.jsx
+++ client/src/Views/Homepage/index.jsx
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
 import { TopMenu } from '../../components/Homepage';
 
-const Home = () => {
-  return (
-    <Segment.Group>
-      <Segment>
-        <TopMenu></TopMenu>
-      </Segment>
+class Home extends Component {
+  state = { uname: '', pasw:''};
 
-      <Segment>
-        <main>
-          <div>
-            <Form size='large'>
-              <Form.Field label='Username' control='input' placeholder='Username' />
-            </Form>
-            <Form size='large'>
-              <Form.Field type='password' label='Password' control='input' placeholder='Password' />
-            </Form>
-            <Button type='submit'>Submit</Button>
-        </div>
-        </main>
-      </Segment>
-    </Segment.Group>
-    
-  );
+  onFormSubmit = (e) =>{
+    console.log(this.state.uname);
+    console.log(this.state.pasw);
+  }
+
+  render(){
+      return (
+        <Segment.Group>
+          <Segment>
+            <TopMenu></TopMenu>
+          </Segment>
+      
+          <Segment>
+            <main>
+              <div>
+                <Form size='large' onSubmit={this.onFormSubmit}>
+                  <Form.Field label='Username' control='input' placeholder='Username' value={this.state.uname} onChange={e=> this.setState({uname: e.target.value})} />
+                  <Form.Field type='password' label='Password' control='input' placeholder='Password' value={this.state.pasw} onChange={e=> this.setState({pasw: e.target.value})}/>
+              
+                  <Button type='submit'>Submit</Button>
+                </Form>
+            </div>
+            </main>
+          </Segment>
+        </Segment.Group>
+            
+      );
+  }
+  
 };
 
 export default Home;
