bdab48726d7f97ee66b9aee24f807f279dc0b6f3
Martyna <baaalbinka@icloud.com>
2020-01-07 17:28:25 +0100

    state

--- client/src/components/Homepage/AddRecipe.jsx
+++ client/src/components/Homepage/AddRecipe.jsx
@@ -2,23 +2,34 @@ import React, { Component } from 'react';
 import { Form, Input, Button } from 'semantic-ui-react';
 
 class AddRecipe extends Component {
+
+  state = {recipeTitle:'', description: '', addIngredient: ''};
+
+
     render() {
+      console.log(this.state);
         return ( 
         <Form>
         <Form.Group widths='equal'>
           <Form.Input 
             fluid label = 'Recipe title' 
             placeholder = 'Enter title'
+            value = {this.state.recipeTitle}
+            onChange={e => {this.setState({ recipeTitle: e.target.value })}}
             required
           />
           <Form.Input 
             fluid label='Description' 
             placeholder='Description'
+            value = {this.state.description}
+            onChange = {e => {this.setState({ description: e.target.value })}}
             required
           />
           <Form.Input 
             fluid label='Add ingredient' 
             placeholder='Enter Ingredient'
+            value = {this.state.addIngredient}
+            onChange = {e => {this.setState({ addIngredient: e.target.value })}}
             required
           />
         </Form.Group>
